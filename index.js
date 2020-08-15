80a78d510f7dac16ab0c9cc9aa0dc3653826803d
xuweifeng <shangguanqingyun@baofu.com>
2020-08-15 09:59:27 +0800

    配置eslint

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-import dvaImg from  './images/dva.png';
-import './style/base.css'
-var img = new Image();
+import dvaImg from './images/dva.png';
+import './style/base.css';
+let img = new Image()
 img.src = dvaImg;
 img.classList.add = 'dva';
 
 document.body.appendChild(img);
+console.log(img);
